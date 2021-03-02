b778dfebecce59e777668b9275b2087c04a1a754
KEH9 <innokkuz@protonmail.com>
2021-03-02 13:05:38 +0500

    1.0.2.

--- js/index.js
+++ js/index.js
@@ -30,6 +30,15 @@ repAmount.addEventListener('blur', repAmountBlurHandler);
 mainTable.addEventListener('click', tableClickHandler);
 questionMark.addEventListener('click', showHelp);
 
+// ----------------------- noSleep -----------------------
+import NoSleep from './NoSleep';
+let noSleep = new NoSleep();
+// Enable wake lock.
+// (must be wrapped in a user input event handler e.g. a mouse or touch handler)
+document.addEventListener('click', function enableNoSleep() {
+  document.removeEventListener('click', enableNoSleep, false);
+  noSleep.enable();
+}, false);
 
 // ----------------------- functions -----------------------
 
